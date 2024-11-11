213d502f058b48a63b31ea8bb36b92c37236105c
zintarh <MAC@Zintarh-MacBook-Pro.local>
2024-11-11 09:48:01 +0100

    chore: changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const TerserPlugin = require("terser-webpack-plugin");
+const nextConfig = {
+  swcMinify: false, // Disable SWC minification temporarily
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.optimization.minimizer.push(
+        new TerserPlugin({
+          terserOptions: {
+            mangle: {
+              reserved: ['t'], // Avoid mangling `t`
+            },
+          },
+        })
+      );
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
