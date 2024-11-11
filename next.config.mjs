6ddfde628b828a55b297bfafab5043976ab28ea7
zintarh <MAC@Zintarh-MacBook-Pro.local>
2024-11-11 11:27:36 +0100

    chore: fix DApp

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,4 @@
 /** @type {import('next').NextConfig} */
-import TerserPlugin from "terser-webpack-plugin";
-
-const nextConfig = {
-  swcMinify: false, // Temporarily disable SWC minification for debugging
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.optimization.minimizer.push(
-        new TerserPlugin({
-          terserOptions: {
-            mangle: {
-              reserved: ["t"], // Prevent mangling of the identifier 't'
-            },
-          },
-        })
-      );
-    }
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
